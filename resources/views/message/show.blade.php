40b96aa2dc9f71446ee17acfd5056193cb382236
Liam Hubers <liam.hubers@gmail.com>
2015-05-04 16:51:59 +0200

    Styled message

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -2,13 +2,38 @@
 
 @section('content')
 
-    <p>Gepost door: {{$message->getProperty('from')->getProperty('name')}}</p>
-    <p>Message: {{$message->getProperty('message')}}</p>
-    <p>Likes: {{$message->likesCount}}</p>
+    <div class="post">
+        <div class="profile">
+            <div class="from">
+                <img src="http://placehold.it/50x50"> <span class="posted-by">Posted by</span> <span class="name">{{$message->getProperty('from')->getProperty('name')}}</span>
+            </div>
+            <div class="when">
+                9 hours ago
+            </div>
+        </div>
+        <div class="message">
+            {{$message->getProperty('message')}}
+        </div>
+        <div class="social">
+            This post has 8 comments and {{$message->likesCount}} likes<br />
+            <button><i class="fa fa-thumbs-up"></i> Comment</button><button><i class="fa fa-thumbs-up"></i> Like</button>
+            <textarea name="comment" class="comment" spellcheck="false">Write a comment...</textarea>
+        </div>
+    </div>
 
-    <p>Reacties:</p>
     @foreach($message->comments as $comment)
-        <p>Likes: {{$comment->getProperty('like_count')}} - {{$comment->getProperty('message')}}</p>
+        <div class="comment">
+            <div class="profile small">
+                <div class="from">
+                    <img src="http://placehold.it/50x50"> <span class="posted-by">Posted by</span> <span class="name">{{$message->getProperty('from')->getProperty('name')}}</span>
+                </div>
+                <div class="when">
+                    9 hours ago
+                </div>
+            </div>
+            {{$comment->getProperty('message')}}
+            Likes: {{$comment->getProperty('like_count')}}
+        </div>
     @endforeach
 
 @stop
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,6 +4,7 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600,400' rel='stylesheet' type='text/css'>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="/styles/template.css" rel="stylesheet">
 </head>
 <body>
